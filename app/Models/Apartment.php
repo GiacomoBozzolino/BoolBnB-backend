e4841d2f895764288fdcd8043209662fd6b281d1
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-09-14 10:07:13 +0200

    [FEAT] Impostati model Apartment e User + slug

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -4,8 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str; 
+
+use App\Models\User;
 
 class Apartment extends Model
 {
     use HasFactory;
+
+    /*connessione con User*/
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'n_rooms',
+        'n_beds',
+        'n_bathrooms',
+        'square_meters',
+        'address',
+        'cover_img',
+        'latitude',
+        'longitude',
+        'visibility',
+        'description',
+    ];
+
+    public static function generateSlug($title){
+        return Str::slug($title, '-');
+    }
 }

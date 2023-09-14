b75eea4a08183d2a4314e5f9fb37ea76a2546de8
EugeniuPeruzzi <jaka1997@live.it>
2023-09-14 23:08:27 +0200

    crerated seeedre for db services

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-use Faker\Generator as Faker;
 use App\Models\Service; 
 
 class ServiceSeeder extends Seeder
@@ -15,15 +14,59 @@ class ServiceSeeder extends Seeder
      *
      * @return void
      */
-    public function run(Faker $faker)
+    public function run()
     {
-        for ($i=1; $i <11 ; $i++) {
-            $service= new Service(); 
-            $service->type= $faker -> word();
-            $service->icon= $faker -> word();
-            
+        $data = [
+            [
+                'type'=>'Parcheggio ', 
+                'icon'=>'<i class="fa-solid fa-square-parking"></i>'
+            ],
+            [
+                'type'=>'Connessione Wifi Gratuita', 
+                'icon'=>'<i class="fa-solid fa-wifi"></i>'
+            ],
+            [
+                'type'=>'Ristorante', 
+                'icon'=>'<i class="fa-solid fa-utensils"></i>'
+            ],
+            [
+                'type'=>'Animali ammessi', 
+                'icon'=>'<i class="fa-solid fa-paw"></i>'
+            ],
+            [
+                'type'=>'Palestra', 
+                'icon'=>'<i class="fa-solid fa-dumbbell"></i>'
+            ],
+            [
+                'type'=>'Camere non fumatori', 
+                'icon'=>'<i class="fa-solid fa-ban-smoking"></i>'
+            ],
+            [
+                'type'=>'Spa & Centro benessere', 
+                'icon'=>'<i class="fa-solid fa-spa"></i>'
+            ],
+            [
+                'type'=>'Piscina', 
+                'icon'=>'<i class="fa-solid fa-person-swimming"></i>'
+            ],
+            [
+                'type'=>'Navetta Aeroportuale', 
+                'icon'=>'<i class="fa-solid fa-van-shuttle"></i>'
+            ],
+            [
+                'type'=>'Reception 24/7', 
+                'icon'=>'<i class="fa-regular fa-clock"></i>'
+            ],
+        ];
 
+
+        foreach ($data as $item) {
+            $service = new Service();
+
+            $service->type = $item['type'];
+            $service->icon = $item['icon'];
             $service->save();
+
         }
     }
 }

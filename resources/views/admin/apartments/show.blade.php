72b3d5a4536ac2753e51ea487abbd5455e64bd45
EugeniuPeruzzi <jaka1997@live.it>
2023-09-14 12:07:55 +0200

    created/done show

---
+++ resources/views/admin/apartments/show.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col d-flex justify-content-center mt-5">
+                <div class="card" style="width: 45rem;">
+                    <img src="{{ $apartment->cover_img }}" class="card-img-top" alt="" width="600px">
+                    <div class="card-body">
+                        <p class="card-title">Titolo: <strong>{{ $apartment->title }}</strong></p>
+                        <p class="card-text">Numero delle stanze: <strong>{{ $apartment->n_rooms }}</strong></p>
+                        <p class="card-text">Numero stanze da letto: <strong>{{ $apartment->n_bedrooms }}</strong></p>
+                        <p class="card-text">Numero bagni: <strong>{{ $apartment->n_bathrooms }}</strong></p>
+                        <p class="card-text">Breve descrizione: <strong>{{ $apartment->description }}</strong></p>
+                        <a href="{{ Route('admin.apartments.index') }}" class="btn btn-primary">Back Home</a>
+                        {{-- <p class="card-text"> {{ $posts->category->name }} </p> --}}
+                        <div class="col-12">
+                            {{-- <strong>Servizi</strong> --}}
+                            {{-- DA INSERIRE TUTTI I SERVIZI A MO DI LABLE --}}
+
+
+                            {{-- @if ($posts->technologies)
+                                @foreach ($posts->technologies as $technology)
+                                    <a href="" class="btn btn-sm btn-primary">{{ $technology->name }}</a>
+                                @endforeach
+                            @else
+                                Non sono presenti tecnologie associate al progetto
+                            @endif --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection

5f30dafbbb17b18d6ab329fb7d6a228a39ef00f9
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-09-14 10:46:25 +0200

    [FEAT] settato seeder con faker/funzionante

--- database/seeders/ApartmentSeeder.php
+++ database/seeders/ApartmentSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Apartment;
+use Faker\Generator as Faker;
+use Faker\Provider\en_US\Address;
 class ApartmentSeeder extends Seeder
 {
     /**
@@ -12,8 +14,26 @@ class ApartmentSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=1; $i <11 ; $i++) {
+            $apartment= new Apartment(); 
+            $apartment->title= $faker -> sentence(3);
+            $apartment->slug=$apartment->generateSlug($apartment->title);
+            $apartment->n_rooms= $faker ->randomDigit();
+            $apartment->n_beds= $faker ->randomDigit();
+            $apartment->n_bathrooms= $faker ->randomDigit();
+            $apartment->square_meters= $faker ->numberBetween(40,250);
+            $apartment->address= $faker -> streetAddress();
+            $apartment->cover_img= $faker -> imageUrl(640,480,'city', true);
+            $apartment->latitude= $faker -> latitude();
+            $apartment->longitude= $faker -> longitude();
+            $apartment->visibility= $faker -> boolean();
+            $apartment->description= $faker -> paragraph(2);
+        
+
+            $apartment->save();
+  
+        }
     }
 }

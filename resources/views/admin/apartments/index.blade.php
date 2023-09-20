53cc5c255eeebfa725188ef764a00efd37d1f5ad
EugeniuPeruzzi <jaka1997@live.it>
2023-09-20 19:03:48 +0200

    modification idenx cards

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -19,59 +19,67 @@
                             <i class="fa-solid fa-plus"></i></a>
                     </div>
                 </div>
-                <div class="border border-light rounded-3 d-flex flex-wrap justify-content-evenly">
+                <div class="border border-light rounded-3 d-flex flex-wrap justify-content-start">
                     {{-- CARDS --}}
                     @foreach ($apartments as $apartment)
-                        <div class="card m-4" style="max-width: 23rem; ">
+                        <div class="m-4 d-flex border rounded-5 p-3" style="width: 40rem; ">
+                            {{-- imagine --}}
                             <div class="ratio ratio-4x3">
-                                <img src="{{ asset('storage/' . $apartment->cover_img) }}" class="card-img-top ratio ratio-4x3"
-                                    alt="{{ $apartment->title }}">
+                                <img src="{{ asset('storage/' . $apartment->cover_img) }}"
+                                    class="card-img-top ratio ratio-4x3 rounded-5" alt="{{ $apartment->title }}">
                             </div>
-                            <div class="card-body">
-                                <h4 class="card-title text-center">{{ $apartment->title }}</h4>
-                                <div class="description-card overflow-auto mt-2" style="height: 12rem">
-                                    <p class="card-text py-2 text-start">{{ $apartment->description }}</p>
+                            {{-- CARD CONTETN --}}
+                            <div class="card-container ms-4">
+                                {{-- text card --}}
+                                <div class="card-body">
+                                    <h4 class="card-title text-center">{{ $apartment->title }}</h4>
+                                    <div class="description-card overflow-auto mt-2" style="height: 12rem">
+                                        <p class="card-text py-2 text-start">{{ $apartment->description }}</p>
+                                    </div>
+                                </div>
+                                {{-- servizi --}}
+                                <ul class="list-group list-group-flush text-center">
+                                    <li class="list-group-item">
+
+                                        @if (count($apartment->services) > 0)
+                                            @foreach ($apartment->services as $item)
+                                                <?php echo $item->icon; ?>
+                                            @endforeach
+                                        @else
+                                            <strong>Non ci sono servizi inseriti</strong>
+                                        @endif
+                                    </li>
+                                </ul>
+                                {{-- tasti di navigazione  --}}
+                                <div class="my-2 d-flex align-items-center justify-content-center">
+                                    <div>
+                                        <a href="{{ route('admin.apartments.show', $apartment->id) }}"
+                                            class="btn mx-1 rounded-5 btn-show">
+                                            <i class="fas fa-eye"></i>
+                                        </a>
+                                    </div>
+
+                                    <div>
+                                        <a href="{{ route('admin.apartments.edit', $apartment->id) }}"
+                                            class="btn rounded-5 btn-modify">
+                                            <i class="fas fa-edit"></i>
+                                        </a>
+                                    </div>
+
+                                    <div>
+                                        <form class="apartment-delete-button d-inline-block mx-1 btn-delete rounded-5"
+                                            data-apartment-title="{{ $apartment->title }}"
+                                            action="{{ route('admin.apartments.destroy', $apartment) }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn ">
+                                                <i class="fas fa-trash"></i>
+                                            </button>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
-                            <ul class="list-group list-group-flush text-center">
-                                <li class="list-group-item">
 
-                                    @if (count($apartment->services) > 0)
-                                        @foreach ($apartment->services as $item)
-                                            <?php echo $item->icon; ?>
-                                        @endforeach
-                                    @else
-                                        <strong>Non ci sono servizi inseriti</strong>
-                                    @endif
-                                </li>
-                            </ul>
-                            <div class="my-2 d-flex align-items-center justify-content-center">
-                                <div>
-                                    <a href="{{ route('admin.apartments.show', $apartment->id) }}"
-                                        class="btn mx-1 rounded-5 btn-show">
-                                        <i class="fas fa-eye"></i>
-                                    </a>
-                                </div>
-
-                                <div>
-                                    <a href="{{ route('admin.apartments.edit', $apartment->id) }}"
-                                        class="btn rounded-5 btn-modify">
-                                        <i class="fas fa-edit"></i>
-                                    </a>
-                                </div>
-
-                                <div>
-                                    <form class="apartment-delete-button d-inline-block mx-1 btn-delete rounded-5"
-                                        data-apartment-title="{{ $apartment->title }}"
-                                        action="{{ route('admin.apartments.destroy', $apartment) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn ">
-                                            <i class="fas fa-trash"></i>
-                                        </button>
-                                    </form>
-                                </div>
-                            </div>
                         </div>
                     @endforeach
                 </div>

31b30a753ea77bfc25da008a3cb6a99a722a6279
EugeniuPeruzzi <jaka1997@live.it>
2023-09-28 11:58:07 +0200

    point-0

--- resources/views/admin/sponsors/index.blade.php
+++ resources/views/admin/sponsors/index.blade.php
@@ -4,7 +4,28 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1>ahwduifnwi</h1>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Price</th>
+                            <th>Duration</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($sponsors as $item)
+                            <tr>
+                                <td>{{ $item->title }}</td>
+                                <td>{{ $item->price }}</td>
+                                <td>{{ $item->duration }}</td>
+                                <td>{{ $item->description }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+
             </div>
         </div>
     </div>

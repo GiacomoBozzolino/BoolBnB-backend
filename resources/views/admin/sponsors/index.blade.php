f109803cde1bfba7ba1e19b17fe70aee1479345e
EugeniuPeruzzi <jaka1997@live.it>
2023-10-01 23:20:40 +0200

    major changes in sposor part

--- resources/views/admin/sponsors/index.blade.php
+++ resources/views/admin/sponsors/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="row row-cols-1 mb-5 my-4 mx-2">
-        <div class="col py-3">
+        <div class="col-12 py-3 text-center">
             <h2>
                 Sponsorships
                 <i class="fa-solid fa-star"></i>
@@ -10,30 +10,31 @@
         </div>
     </div>
 
-    <div class="container h-100 d-flex align-items-center justify-content-center p-lg-0">
-        <div class="row w-100 gy-5 mt-5 mt-md-0">
-            @foreach ($sponsors as $sponsor)
-                <div class="col-12 col-lg-4  d-flex justify-content-center">
-                    <a href="{{ route('admin.sponsors.show', [$sponsor->id, 'apartment_id' => $apartment_id]) }}"
-                        class="text-decoration-none w-100">
-                        <div class="my-card">
-                            <div class="content d-flex flex-column">
-                                <h2
-                                    class="card-title text-center fw-bold  {{ strtolower(explode(' ', $sponsor->title)[1]) }} px-2 ">
-                                    {{ explode(' ', $sponsor->title)[1] }}
-                                </h2>
-                                <div class="details ">
-                                    <div class="data">
-                                        <h5 class=" text-center py-2">Prezzo: {{ $sponsor->price }} €</h5>
-                                        <h6 class=" text-center py-2">Durata: {{ $sponsor->duration }} h</h6>
-                                        <p class="card-text text-center py-2 px-3">{{ $sponsor->description }}</p>
-                                    </div>
-                                </div>
-                            </div>
+    <div class="container  p-lg-0">
+        <div class="row mt-5 mt-md-0">
+            {{-- le sponsorizzazioni disponibili --}}
+            <div class="col-12 d-flex justify-content-center">
+
+                @foreach ($sponsors->reverse() as $sponsor)
+                    <div class=" me-5 rounded-5 shadow hvr" style="width: 18rem;">
+                        <div class="text-center rounded-5 py-5 {{ $sponsor->id == 3 ? 'bg-warning' : 'bg-grey' }}">
+                            <h5 class="">{{ explode(' ', $sponsor->title)[1] }}</h5>
+                            <h1>&euro; {{ $sponsor->price }}</h1>
                         </div>
-                    </a>
-                </div>
-            @endforeach
+                        <div class="card-body px-2 my-4 d-flex flex-column align-items-center">
+                            <div class="mb-3">
+                                <i class="fa-regular fa-clock"></i>
+                                <h5 class="d-inline">Durata {{ $sponsor->duration }}h</h5>
+                            </div>
+                            <p class="text-center" style="height: 130px;">{{ $sponsor->description }}</p>
+                        </div>
+                        <div class="d-flex justify-content-center mb-5">
+                            <a href="{{ route('admin.sponsors.show', [$sponsor->id, 'apartment_id' => $apartment_id]) }}"
+                                class="text-decoration-none btn-sponsor text-uppercase px-3 py-2 {{ $sponsor->id == 3 ? 'bg-warning' : 'border border-2 pay-hover' }}">Acquista</a>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
